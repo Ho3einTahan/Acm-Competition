b99fd5eb94943b8c1cf60f480e70bc5d571a4ccf
ho3eintahan <hoseintahan84@gmail.com>
2024-12-23 21:04:36 +0330

    Update diamond.cpp

--- diamond.cpp
+++ diamond.cpp
@@ -15,8 +15,24 @@ int main()
         {
             cout << " ";
         }
-    
-        for (int j = 0; j < i * 2 - 1; j++)
+
+        for (int j = 1; j <= i * 2 - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+    for (int i = n; i >= 1; i--)
+    {
+
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i * 2 - 1; j++)
         {
             cout << "*";
         }
